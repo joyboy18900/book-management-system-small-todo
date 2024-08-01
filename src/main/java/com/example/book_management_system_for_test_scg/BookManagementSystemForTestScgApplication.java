94deca199dd8c1d223ee9d96f743da0476e53012
joyboy18900 <surakiat.dev@gmail.com>
2024-08-01 22:01:01 +0700

    Init projects

---
+++ src/main/java/com/example/book_management_system_for_test_scg/BookManagementSystemForTestScgApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_management_system_for_test_scg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSystemForTestScgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementSystemForTestScgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book_management_system_for_test_scg/BookManagementSystemForTestScgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_management_system_for_test_scg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemForTestScgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
